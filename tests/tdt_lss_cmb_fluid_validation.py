877135487ffece66052d1cc3fcbbbacfbe1ca0f5
PJHkorea <zenoforever@naver.com>
2026-09-23 07:26:08 +0900

    Update comment to reflect code changes for damping

--- tests/tdt_lss_cmb_fluid_validation.py
+++ tests/tdt_lss_cmb_fluid_validation.py
@@ -1,4 +1,4 @@
-# 복사 저항(Radiation Drag)에 의한 누적 감쇄(Damping) 및 유체 역학적 변형'을 고려하지 않은 순수 기하학적 파동 공식
+# 복사 저항(Radiation Drag)에 의한 누적 감쇄(Damping) 및 유체 역학적 변형을 한 상태의 코드
 
 import numpy as np
 import pandas as pd
